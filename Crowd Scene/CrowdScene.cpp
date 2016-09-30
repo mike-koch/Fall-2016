d1f0ed3ef688327ef812174919e79eb4d54d605f
Mike Koch <mkoch227@gmail.com>
2016-09-30 00:14:44 -0400

    Rows of cows now shown, move up and down and change material

--- Crowd Scene/CrowdScene.cpp
+++ Crowd Scene/CrowdScene.cpp
@@ -21,16 +21,51 @@ lightPositionLocation, ambientLightColorLocation,
 diffuseLightColorLocation, specularLightColorLocation,
 specularPowerLocation, eyePositionLocation, normalTransformLocation, materialAmbientLocation, materialDiffuseLocation, materialEmissiveLocation, materialSpecularLocation, materialShininessLocation;
 float rotationAngle;
-bool elements;
 int nbrTriangles;
 
+const unsigned int NUMBER_OF_FRAMES_TO_PEAK = 45;
+bool rotationIsClockwise = true;
+unsigned int numberOfFrames = 0;
+
+// This is purely a magic number: the max number of rows before the cows start turning around
+const unsigned int TOTAL_NUMBER_OF_ROWS_OF_COWS = 8;
+
+// Choosing a row value beyond the max row number to prevent accidentally moving a row on start
+int currentRowBeingMoved = TOTAL_NUMBER_OF_ROWS_OF_COWS;
+int previousRow = TOTAL_NUMBER_OF_ROWS_OF_COWS;
+
+void handleKeypress(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		rotationAngle -= 1.0f;
+		break;
+	case GLUT_KEY_RIGHT:
+		rotationAngle += 1.0f;
+		break;
+	default:
+		break;
+	}
+}
+
 void timer(int value) {
-	rotationAngle -= 1.0f;
+	// Add to the # of frames. If at NUMBER_OF_FRAMES_TO_PEAK, start moving the original row of cows down, and start raising the next row
+	if (++numberOfFrames == NUMBER_OF_FRAMES_TO_PEAK) {
+		numberOfFrames = 0;
+		previousRow = currentRowBeingMoved;
+		currentRowBeingMoved = currentRowBeingMoved - 1 == -1 
+			? 7 
+			: currentRowBeingMoved - 1;
+		cout << "Row " << currentRowBeingMoved << " of cows is moving " << endl;
+	}
+
+	// Keyboard check. If the user is holding an arrow key, rotate negative for left / positive for right. Need to use SpecialFunc instead of KeyboardFunc since I'm looking for arrow keys
+	glutSpecialFunc(handleKeypress);
+
 	glutPostRedisplay();
 	glutTimerFunc(1000 / 60, timer, 1);
 }
 
-// The main initialization logic. Most of it is inspired from IcosahedronExample
+// The main initialization logic. Most of the boilerplate code is similar to the sample provided.  However things change quite a bit once the cows are being rendered.
 void init() {
 	float *fileTriangles;
 	float *normalArray;
@@ -93,7 +128,7 @@ void init() {
 }
 
 void display() {
-
+	/* MAIN SETUP HERE */
 	static GLfloat red[4] = { 1.0f, 0.0f, 0.0f, 1.0f },
 		white[4] = { 1.0f, 1.0f, 1.0f, 1.0f },
 		green[4] = { 0.0f, 1.0f, 0.0f, 1.0f },
@@ -112,114 +147,88 @@ void display() {
 	vmath::mat4 currentMatrix = vmath::scale(0.08f) *
 		vmath::rotate(rotationAngle, 0.0f, 1.0f, 0.0f);
 
-	/*
-	* m seems to be wrong.  Need to go back and make sure I've got these in the
-	* right positions.
-	*/
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			normalTransformation[i][j] = currentMatrix[i][j];
 		}
 	}
 
-	// Parent cow #1
-	glUniformMatrix4fv(transformLocation, 1, GL_TRUE, currentMatrix);
 	glUniformMatrix3fv(normalTransformLocation, 1, GL_TRUE,
 		(const GLfloat *)normalTransformation);
-	glUniform4fv(colorLocation, 1, white);
 	glUniform3fv(lightPositionLocation, 1, lightPosition);
 	glUniform3fv(diffuseLightColorLocation, 1, diffuseLightColor);
 	glUniform3fv(ambientLightColorLocation, 1, (const GLfloat *)&ambientLightColor);
 	glUniform3fv(eyePositionLocation, 1, eyePosition);
 	glUniform3fv(specularLightColorLocation, 1, specularLightColor);
 	glUniform1fv(specularPowerLocation, 1, specularPower);
+	/* END MAIN SETUP */
 
-	GLfloat ambient[] = { 0.19225f, 0.0735f, 0.0225f };
-	GLfloat diffuse[] = { 0.7038f, 0.27048f, 0.0828f };
-	GLfloat specular[] = { 0.256777f, 0.137622f, 0.086014f };
-	GLfloat shininess = 12.8f;
-
-	glUniform3fv(materialDiffuseLocation, 1, diffuse);
-	glUniform3fv(materialAmbientLocation, 1, ambient);
-	glUniform3fv(materialSpecularLocation, 1, specular);
-	glUniform1fv(materialShininessLocation, 1, &shininess);
-
-	glBindVertexArray(vertexBuffers[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, arrayBuffers[0]);
-	glDrawArrays(GL_TRIANGLES, 0, nbrTriangles * 3);
-
-
-	// Child cow #1
-	vmath::mat4 translation(vmath::vec4(1.0f, 0.0f, 0.0f, 15.00f),
-		vmath::vec4(0.0f, 1.0f, 0.0f, -3.40f),
-		vmath::vec4(0.0f, 0.0f, 1.0f, 0.00f),
-		vmath::vec4(0.0f, 0.0f, 0.0f, 1.0f));
-	currentMatrix = translation * vmath::scale(0.04f) * vmath::rotate(-rotationAngle, 0.0f, 1.0f, 0.0f);
-
-	glUniformMatrix4fv(transformLocation, 1, GL_TRUE, currentMatrix);
-	glUniform4fv(colorLocation, 1, red);
-
-	GLfloat ambient2[] = { 0.25f, 0.20725f, 0.20725f };
-	GLfloat diffuse2[] = { 1.0f, 0.829f, 0.829f };
-	GLfloat specular2[] = { 0.296648f, 0.296648f, 0.296648f };
-	GLfloat shininess2 = (128.0f*0.088f);
-
-	glUniform3fv(materialDiffuseLocation, 1, diffuse2);
-	glUniform3fv(materialAmbientLocation, 1, ambient2);
-	glUniform3fv(materialSpecularLocation, 1, specular2);
-	glUniform1fv(materialShininessLocation, 1, &shininess2);
-
-	glBindVertexArray(vertexBuffers[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, arrayBuffers[0]);
-	glDrawArrays(GL_TRIANGLES, 0, nbrTriangles * 3);
+	float rowBaseline = -13.0f;
+	float rowOffsetZ = 0.0f;
+	int row = 0;
+	for (int i = 0; i < TOTAL_NUMBER_OF_ROWS_OF_COWS * 8; i++) {
+		if (i > 0 && i % 8 == 0) {
+			//-- We're going to be moving back and switching to the other offset position.
+			rowBaseline = rowBaseline > -14.9f ? -15.0f : -13.0f;
+			rowOffsetZ += 3.0f;
+			row++;
+		}
+		GLfloat yPosition = -14.00f + (float)(4 * row);
+		GLfloat zPosition = 15.0f - (float)rowOffsetZ;
 
 
-	// Child cow #2
-	vmath::mat4 translation3(vmath::vec4(1.0f, 0.0f, 0.0f, 15.00f),
-		vmath::vec4(0.0f, 1.0f, 0.0f, -3.40f),
-		vmath::vec4(0.0f, 0.0f, 1.0f, -10.00f),
-		vmath::vec4(0.0f, 0.0f, 0.0f, 1.0f));
-	currentMatrix = translation3 * vmath::scale(0.04f) * vmath::rotate(-rotationAngle, 0.0f, 1.0f, 0.0f);
+		// Materials: Pearl
+		GLfloat ambient[] = { 0.25f, 0.20725f, 0.20725f };
+		GLfloat diffuse[] = { 1.0f, 0.829f, 0.829f };
+		GLfloat specular[] = { 0.296648f, 0.296648f, 0.296648f };
+		GLfloat shininess = (128.0f*0.088f);
 
-	glUniformMatrix4fv(transformLocation, 1, GL_TRUE, currentMatrix);
-	glUniform4fv(colorLocation, 1, green);
+		if (currentRowBeingMoved == row) {
+			// Z Position should now be  3.0f * (numberOfFrames/60)
+			zPosition = 15.0f - (rowOffsetZ - 3.0f * ((float)numberOfFrames / NUMBER_OF_FRAMES_TO_PEAK));
+			ambient[0] = ambient[1] = ambient[2] = 0.25f;
+			diffuse[0] = diffuse[1] = diffuse[2] = 0.4f;
+			specular[0] = specular[1] = specular[2] = 0.774597f;
+			shininess = (128.0f*0.06f);
+		}
+		else if (previousRow == row) {
+			// Move the row back to their original spot by preforming the reverse of the if block above.
+			zPosition = 15.0f - (rowOffsetZ - 3.0f * ((float)(NUMBER_OF_FRAMES_TO_PEAK - numberOfFrames) / NUMBER_OF_FRAMES_TO_PEAK));
 
-	//Emerald:
-	//http://devernay.free.fr/cours/opengl/materials.html
-	GLfloat ambient4[] = { 0.0215f, 0.1745f, 0.0215f };
-	GLfloat diffuse4[] = { 0.07568f, 0.61424f, 	0.07568f };
-	GLfloat specular4[] = { 0.633f, 0.727811f, 0.633f };
-	GLfloat shininess4 = 128.0f*0.6f;
+			ambient[0] = 0.1f;
+			ambient[1] = 0.18725f;
+			ambient[2] = 0.1745f;
+			diffuse[0] = 0.396f;
+			diffuse[1] = 0.74151f;
+			diffuse[2] = 0.69102f;
+			specular[0] = 0.297254f;
+			specular[1] = 0.30829f;
+			specular[2] = 0.306678f;
+			shininess = 128.0f*0.1f;
+		}
 
-	glUniform3fv(materialDiffuseLocation, 1, diffuse4);
-	glUniform3fv(materialAmbientLocation, 1, ambient4);
-	glUniform3fv(materialSpecularLocation, 1, specular4);
-	glUniform1fv(materialShininessLocation, 1, &shininess4);
+		GLfloat xPosition = rowBaseline + (float)(4 * (i % 8));
 
-	glBindVertexArray(vertexBuffers[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, arrayBuffers[0]);
-	glDrawArrays(GL_TRIANGLES, 0, nbrTriangles * 3);
+		//-- Actually render the cow
+		vmath::mat4 translation(vmath::vec4(1.0f, 0.0f, 0.0f, yPosition), // last value affects the cow's Y position
+			vmath::vec4(0.0f, 1.0f, 0.0f, zPosition), // last value affects the cow's Z position
+			vmath::vec4(0.0f, 0.0f, 1.0f, xPosition), // last value affects the cow's X position
+			vmath::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+		currentMatrix = translation * vmath::scale(0.05f) * vmath::rotate(rotationAngle, 0.0f, 1.0f, 0.0f);
 
-	// Parent cow #2
-	vmath::mat4 translation2(vmath::vec4(1.0f, 0.0f, 0.0f, 0.00f),
-		vmath::vec4(0.0f, 1.0f, 0.0f, 0.00f),
-		vmath::vec4(0.0f, 0.0f, 1.0f, -5.0f),
-		vmath::vec4(0.0f, 0.0f, 0.0f, 1.0f));
-	currentMatrix = translation2 * vmath::scale(0.08f) * vmath::rotate(rotationAngle, 0.0f, 1.0f, 0.0f);
-	glUniformMatrix4fv(transformLocation, 1, GL_TRUE, currentMatrix);
-	glUniform4fv(colorLocation, 1, blue);
-	// silver
-	GLfloat ambient3[] = { 0.19225f, 0.19225f, 0.19225f };
-	GLfloat diffuse3[] = { 0.50754f, 0.50754f, 0.50754f };
-	GLfloat specular3[] = { 0.508273f, 0.508273f, 0.508273f };
-	GLfloat shininess3 = (128.0f * 0.4f);
+		glUniformMatrix4fv(transformLocation, 1, GL_TRUE, currentMatrix);
+		glUniform4fv(colorLocation, 1, red);
 
-	glUniform3fv(materialDiffuseLocation, 1, diffuse3);
-	glUniform3fv(materialAmbientLocation, 1, ambient3);
-	glUniform3fv(materialSpecularLocation, 1, specular3);
-	glUniform1fv(materialShininessLocation, 1, &shininess3);
 
-	glDrawArrays(GL_TRIANGLES, 0, nbrTriangles * 3);
+		glUniform3fv(materialDiffuseLocation, 1, diffuse);
+		glUniform3fv(materialAmbientLocation, 1, ambient);
+		glUniform3fv(materialSpecularLocation, 1, specular);
+		glUniform1fv(materialShininessLocation, 1, &shininess);
+
+		glBindVertexArray(vertexBuffers[0]);
+		glBindBuffer(GL_ARRAY_BUFFER, arrayBuffers[0]);
+		glDrawArrays(GL_TRIANGLES, 0, nbrTriangles * 3);
+	}
 
 	glFlush();
 }
@@ -229,21 +238,9 @@ void display() {
 * sets up the window.
 */
 int main(int argCount, char *argValues[]) {
+	// Start facing the cows head-on
+	rotationAngle = -90.0f;
 	glutInit(&argCount, argValues);
-
-	// Check for a flag to determine if we are using glDrawElements or 
-	// glDrawArray.  I'm looking for either --elements or --array.
-	// The default is the elements.
-	elements = true;
-	for (int argNbr = 1; argNbr < argCount; argNbr++) {
-		if (argValues[argNbr] == "--elements") {
-			cout << "Drawing using glDrawElements" << endl;
-			elements = true;
-		}
-		else if (argValues[argNbr] == "--array") {
-			elements = false;
-		}
-	}
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("Crowd Scene");
@@ -253,7 +250,7 @@ int main(int argCount, char *argValues[]) {
 	glewInit();
 	init();
 	glutDisplayFunc(display);
-	glutTimerFunc(1000 / 30, timer, 1);
+	glutTimerFunc(1000 / 60, timer, 1);
 	glutMainLoop();
 	return 0;
 }
